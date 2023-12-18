f1e9285271e3ba9a96a8c33eed230b1eb2c975ae
hamid321 <hamidazim321@gmail.com>
2023-12-18 21:49:03 +0500

    Add setters for source and label

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,6 @@
 class Item
-  attr_accessor :gener, :author, :source, :label
-  attr_reader :id, :publish_date, :archived
+  attr_reader :genre, :author, :source, :label,
+  attr_accessor  :id, :publish_date, :archived
 
   def initialize(id, publish_date, archived)
     @id = id
@@ -8,11 +8,23 @@ class Item
     @archived = archived
   end
 
-  def save_genre(genre)
+  def save_genre=(genre)
     @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
   end
 
-  def save_author(author)
+  def save_author=(author)
     @author = author
+    author.add_item(self) unless author.items.include?(self)
+  end
+
+  def save_source=(source)
+    @source = source
+    source.add_item(self) unless source.items.include?(self)
+  end
+
+  def save_label=(label)
+    @label = label
+    label.add_item(self) unless label.items.include?(self)
   end
 end
