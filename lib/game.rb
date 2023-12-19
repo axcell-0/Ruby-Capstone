86e5d636c57e9345ed501864b6f0dc7565cd5d12
user-faiz <email2faizm@gmail.com>
2023-12-19 19:42:13 +0530

    fixed linters error

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require_relative 'item'
+require 'date'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :id
@@ -8,10 +9,9 @@ class Game < Item
     @id = Random.rand(1..1000)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
-    @publish_date = publish_date
   end
 
   def can_be_archived?
-    super && last_played_at && (Date.today - last_played_at) > 2.years
-
+    super && @last_played_at && (Date.today - Date.parse(@last_played_at)) > 2 * 365
   end
+end
