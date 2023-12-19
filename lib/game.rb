7b48f56df1a493d7ca948751ee82b857601c6823
user-faiz <email2faizm@gmail.com>
2023-12-19 19:40:20 +0530

    update game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,17 @@
-# game.rb
-
 require_relative 'item'
 
 class Game < Item
-  def initialize(publish_date)
+  attr_accessor :multiplayer, :last_played_at, :id
+
+  def initialize(multiplayer, last_played_at, publish_date)
     super(publish_date)
+    @id = Random.rand(1..1000)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+    @publish_date = publish_date
   end
 
   def can_be_archived?
-    super
+    super && last_played_at && (Date.today - last_played_at) > 2.years
+
   end
-end
