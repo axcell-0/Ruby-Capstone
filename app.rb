180535226ad73df10466be155dd02aca4e247215
hamid321 <hamidazim321@gmail.com>
2023-12-19 19:46:07 +0500

    Add function to list all genres

--- app.rb
+++ app.rb
@@ -7,29 +7,16 @@ require 'fileutils'
 require './modules/genre_module'
 class App
   include GenreModule
-  private
-  def check_file(path)
-    return if Dir.exist?(path)
-    FileUtils.touch(path)
+  def add_genre
+    create_genre
   end
-
-  public
-  def store_genre(genre)
-    path = './data/genre.json'
-    check_file(path)
-    write_genre_to_file(genre)
+  def list_genre
+    show_all_genres
   end
 end
 
-genre = Genre.new('Rock')
-genre2 = Genre.new('pock')
-
-genre.items.push(Item.new('2023-12-12'))
-genre2.items.push(Item.new('2023-12-12'))
 
 app = App.new
-app.store_genre(genre2)
-app.store_genre(genre)
-
+app.create_genre
 
 
--- modules/genre_module.rb
+++ modules/genre_module.rb
@@ -1,9 +1,35 @@
+require './lib/genre'
 module GenreModule
+  PATH = './data/genre.json'
+  def check_file()
+    return if Dir.exist?(PATH)
+    FileUtils.touch(PATH)
+  end
   def write_genre_to_file(genre)
-    path = './data/genre.json'
-    file = File.read(path)
+    check_file
+    file = File.read(PATH)
     data = file.empty? ? [] : JSON.parse(file)
     data.push(JSON.parse(genre.to_json))
-    File.write(path, JSON.pretty_generate(data))
+    File.write(PATH, JSON.pretty_generate(data))
+  end
+
+  def create_genre
+    print 'Name of Genre: '
+    name = gets.chomp
+    genre = Genre.new(name)
+    write_genre_to_file(genre)
+    puts 'Genre Created Successfully!'
+  end
+
+  def show_all_genres
+    if !File.exist?(PATH) || File.read(PATH).empty?
+      puts 'No genres have been added yet'
+      return
+    end    
+    data = JSON.parse(File.read(PATH))
+    puts 'Genres:'
+    data.foreach do |genre|
+      " #{genre.name}"
+    end
   end
 end
