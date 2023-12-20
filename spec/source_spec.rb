9ba3100f3576773846a4af8a5dd35846ec37aa61
Samuel <samif3443@gmail.com>
2023-12-20 10:32:12 +0300

    Add spec folder

---
+++ spec/source_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../classes/source_class'
+
+describe Source do
+  before(:each) do
+    @source = Source.new('Source Name')
+    @item = double('Item')
+  end
+
+  it 'has an id' do
+    expect(@source).to be_an_instance_of Source
+  end
+
+  it 'has a name' do
+    expect(@source.name).to eq('Source Name')
+  end
+
+  it 'can add an item' do
+    allow(@item).to receive(:source).and_return(@source)
+    @source.add_item(@item)
+    expect(@source.items).to include(@item)
+  end
+end
