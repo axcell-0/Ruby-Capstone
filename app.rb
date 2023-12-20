ec5c71acc34dfe8ce7124b824ffe3e544c0ec082
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-12-20 09:10:36 +0100

    Modify app.rb

--- app.rb
+++ app.rb
@@ -29,13 +29,30 @@ class App
   def add_music_album
     create_music_album
   end
-  def list_music_album
-    show_all_music_albums
+
+  def list_all_books
+    list_books
+  end
+
+  def list_all_labels
+    list_labels
+  end
+
+  def add_a_book
+    puts 'Enter the book\'s publish date (YYYY-MM-DD):'
+    date = gets.chomp
+    puts 'Enter the book\'s publisher:'
+    publisher = gets.chomp
+    puts "Select label:\n1. Gift\n2. New\n"
+    label_choice = gets.chomp.to_i
+    puts 'Enter the book\'s label color:'
+    label_color = gets.chomp
+    print "Select cover state: \n1. Good \n2. Bad\n"
+    cover_state_choice = gets.chomp.to_i
+    add_new_book(date, publisher, cover_state_choice, label_choice, label_color)
   end
 end
 
 
 app = App.new
-app.list_music_album
-
-
+app.add_music_album
