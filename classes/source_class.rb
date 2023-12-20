2986b27f3913f82a3a017b5870b4e38538567d5c
Samuel <samif3443@gmail.com>
2023-12-20 10:39:25 +0300

    Add source_class.rb to classes folder

---
+++ classes/source_class.rb
@@ -0,0 +1,23 @@
+class Source
+    attr_reader :id, :name
+    attr_accessor :items
+  
+    def initialize(name)
+      @id = Random.rand(1...100)
+      @name = name
+      @items = []
+    end
+  
+    def add_item(item)
+      @items << item
+      item.add_source(self) unless item.source == self
+    end
+  
+    def to_h
+      {
+        id: @id,
+        name: @name
+      }
+    end
+  end
+  
