3e63f8e8c1dc134428191d1e346b7bc65d2671a4
Faiz Mohammad <email2faizm@gmail.com>
2023-12-21 18:12:32 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -29,6 +29,28 @@ CREATE TABLE genres (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE games (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  genre_id INT NOT NULL,
+  author_id INT NOT NULL,
+  label_id INT NOT NULL,
+  multiplayer BOOLEAN NOT NULL,
+  last_played_at DATE NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres (id),
+  CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors (id),
+  CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES labels (id)
+);
+
+CREATE TABLE authors (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR (255) NOT NULL,
+  last_name VARCHAR (255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE Movie (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     silent BOOLEAN,
